5b62163e5f33ad61f9026fc1afb8cc1f8f090893
kdzh899 <kang.zhao@astrazeneca.com>
2023-04-29 01:47:27 +0800

    str instead of list

--- main.py
+++ main.py
@@ -30,33 +30,33 @@ class HiddenPrints:
 
 if __name__ == '__main__':
     try:
-        pwds = []
+        pwds = ''
         total = 4
         print(f"请新娘设置金库{total}位密码：")
         for i in range(total):
-            pwds.append(read_password(f"第{i + 1}位"))
+            pwds += read_password(f"第{i + 1}位")
         print("设置完成，密码已保存至`pwds`中!!!")
     except (KeyboardInterrupt, EOFError) as err:
         pass
     else:
-        print(f"\n\n新郎求解金库{total}位密码：")
-        print(f"并将结果按位存到`answer`变量中")
-        answer = ''
+        print(f"\n\n!!!新郎求解金库{total}位密码!!!")
+        print(f"将猜测存到`guess`变量中")
+        guess = ''
         __counter = 0
         while True:
             try:
                 while True:
                     __counter += 1
-                    print(f"请进行第{__counter}次尝试")
+                    print(f"开始第{__counter}次尝试...")
                     with HiddenPrints():
                         import pdb
                         pdb.set_trace()
-                        if answer == pwds:
+                        if guess == pwds:
                             break
             except (KeyboardInterrupt, EOFError, BdbQuit) as err:
                 continue
             else:
                 print(f"恭喜求解完成{'!'*(i+1)}")
-                print(f"密码是{''.join(pwds))}")
+                print(f"密码是：{pwds}")
                 break
 
