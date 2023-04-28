d968f724f07a3c5c6506190982d7a4d7cf7aa884
kdzh899 <kang.zhao@astrazeneca.com>
2023-04-29 01:26:19 +0800

    init

---
+++ main.py
@@ -0,0 +1,62 @@
+from bdb import BdbQuit
+import sys
+import os
+
+def read_password(help_text=''):
+    """
+    Read password from stdin
+    """
+    import getpass
+    while True:
+        if help_text:
+            print(help_text)
+        pw = getpass.getpass("密码：")
+        if len(pw) > 1:
+            print("请输入一位字符")
+        elif pw:
+            break
+    return pw
+
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+
+if __name__ == '__main__':
+    try:
+        pwds = []
+        total = 4
+        print(f"请新娘设置金库{total}位密码：")
+        for i in range(total):
+            pwds.append(read_password(f"第{i + 1}位"))
+        print("设置完成，密码已保存至`pwds`中!!!")
+    except (KeyboardInterrupt, EOFError) as err:
+        pass
+    else:
+        print(f"\n\n新郎求解金库{total}位密码：")
+        print(f"并将结果按位存到`answer`变量中；输入`c`回车检查")
+        answer = ''
+        __counter = 0
+        while True:
+            try:
+                while True:
+                    __counter += 1
+                    print(f"请进行第{__counter}次尝试")
+                    with HiddenPrints():
+                        import pdb
+                        pdb.set_trace()
+                        if answer == pwds:
+                            break
+            except (KeyboardInterrupt, EOFError, BdbQuit) as err:
+                continue
+            else:
+                print(f"恭喜求解完成{'!'*(i+1)}")
+                print(f"密码是{''.join(pwds))}")
+                break
+
